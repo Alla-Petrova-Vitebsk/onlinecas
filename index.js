0d16df02e6441e97db3b2b22331411d6db8fafa9
Alla-Petrova-Vitebsk <alla_petrova@mail.ru>
2024-10-01 20:05:15 +0300

    add feature

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ accordion();
 
 function accordion(){
     const acc = document.getElementsByClassName("acc_title");
-
+console.log(acc)
     for (let i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
             this.classList.toggle("acc_active");
     
             let panel = this.nextElementSibling;
+            
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
